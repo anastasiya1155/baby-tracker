e55d21cfb80d1038f299c93b4599e2eb26e4eaca
anastasiya1155 <anastasiya1155@gmail.com>
2026-01-12 13:20:59 +0100

    Done! Times now display in 24-hour format (e.g., "14:30 - 15:45" instead of "02:30 PM - 03:45 PM").

--- src/utils/formatting.ts
+++ src/utils/formatting.ts
@@ -1,9 +1,10 @@
 import { ActivitySubcategory } from '../types'
 
 export function formatTime(date: Date): string {
-  return date.toLocaleTimeString('en-US', {
+  return date.toLocaleTimeString('en-GB', {
     hour: '2-digit',
     minute: '2-digit',
+    hour12: false,
   })
 }
 
